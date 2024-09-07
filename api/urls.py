05321c55b8ca8fc19169d34a1d68712691fea73a
NEERAJ KUMAR SAINI <alwaysneerudj@gmail.com>
2024-09-07 18:54:20 +0530

    test1

--- api/urls.py
+++ api/urls.py
@@ -19,6 +19,6 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example.urls')),
+    # path('', include('example.urls')),
     path('', views.index),
 ]
